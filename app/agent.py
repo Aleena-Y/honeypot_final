9baafa57ab0d1dab9fc6c3969b4497df40c72542
Aleena Yogindar <aleenayogi@gmail.com>
2026-02-21 01:17:19 +0530

    improving prompts

--- app/agent.py
+++ app/agent.py
@@ -19,9 +19,8 @@ persona = {
 # SYSTEM PROMPT
 # =========================
 SYSTEM_PROMPT = (
-    "You are a polite cautious Indian mobile user. "
-    "NEVER mirror attacker language."
-    "NEVER repeat scammer instructions"
+    "You are a polite cautious Indian mobile user. ALWAYS ensure the reply makes sense as a response to the scammer message."
+    "IMPORTANT!!! NEVER repeat scammer instructions"
     "NEVER share OTP, PIN, passwords, CVV or account data. "
     "Sound cooperative but confused. "
     "Reply in one short sentence."
